0246e3745ab1211253aed416ce418197d92dd605
alebus <alebus@SB-ALEBUS.corp.valueclick.com>
2015-05-07 15:12:40 -0700

    error_checking branch

---
+++ public_html/TODO-and-NOTES.js
@@ -0,0 +1,46 @@
+/* 
+*
+*
+*
+*I moved notes and GENERAL TODO into here on 05-07-2015
+*
+*
+*PUSHING TEST VERSIONS:
+*I created a separate dir on Shell 1
+*just SFTP whatever you want into there, ensure you have the correct dir
+*HAVE PEOPLE USE THAT before you push that version live
+*
+*
+*Version: 1.2: major updates to HTML and output format, plus a move to shell1 from allied
+*Version: 1.3: move into netbeans, move to github, etc etc
+*05-05-2015 I removed popup which included changing the CSS
+*version 1.4: 05-05-2015 and 05-06-2015: tons of changes to add various error checking, etc etc etc
+*also supports encoded ampersands now, changed for loop structure, etc etc
+*
+*TODO how best keep track of version numbers? or don't?
+*
+*TODO do you need to escape input etc? esp if something located on external server, I don't want them doing XSS attacks etc
+*JS/jQuery book has some great info on that 
+*
+**TODO you could have it display every param that is NOT in a list of known params
+*
+*TODO see notes in Onenote
+*TODO is there a way to put comments in a separate private file etc? or just make a TODO file and don't push that to production
+*or branch it and remove all comments and push that one to the server
+*
+*TODO sometime closing the window redirects the browser
+*
+*TODO remove comments before push to production
+*
+*TODO - parse &amp; too
+*
+*TODO update to match kibana version as needed but remember that a lot of this works differently 
+*
+*TODO do these next: 
+**Per jen: amtx or other wrong things
+*DONE *per jen: chars other than dashes/underscores, a-z and 0-9 in SKUs
+*
+*TODO soon - check for anything besides decimal and numbers in AMT / AMOUNT 
+*
+*/
+
